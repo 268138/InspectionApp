ab0817bd8e4c90ef2395b86dfc5a53b73dc94d79
Arnas <arnas.leu@gmail.com>
2025-04-14 19:55:52 +0300

    initial commit

---
+++ src/test/java/org/example/inspectionapplication/InspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.inspectionapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
